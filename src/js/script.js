f215ab83ddf7d2f1d0673f4285c223eb061bcf1a
Heitor <heitorfialhon@gmail.com>
2025-06-12 14:07:35 -0300

    added a hamburger menu to mobile and changed the structure of the header

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,12 @@
 function buyTickets(){
     window.location.href = "tickets.html";
 }
+
+// Menu toggle setting for media query
+
+const toggle = document.getElementById('menu-toggle');
+const navLinks = document.getElementById('nav-links');
+
+toggle.addEventListener('click',()=>{
+    navLinks.classList.toggle('active');
+});
