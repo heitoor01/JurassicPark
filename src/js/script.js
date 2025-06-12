c7c08bc520e22e7dc11c9f12da17dc403bb9e321
Heitor <heitorfialhon@gmail.com>
2025-06-12 20:14:33 -0300

    fix: fixed the bug that prevented the nav-links to be closed by clicking outside the box in mobile

--- src/js/script.js
+++ src/js/script.js
@@ -7,6 +7,18 @@ function buyTickets(){
 const toggle = document.querySelector('#menu-toggle');
 const navLinks = document.querySelector('.nav-links');
 
-toggle.addEventListener('click',()=>{
+// Close/Open button
+toggle.addEventListener('click', (event)=>{
+    event.stopPropagation(); // Prevent immediate close
     navLinks.classList.toggle('active');
 });
+
+// Close clicking anywhere on the screen
+document.addEventListener('click', (event) =>{
+    const isClickingInsideMenu = navLinks.contains(event.target);
+    const isClickingOnToggle = toggle.contains(event.target);
+
+    if(!isClickingInsideMenu && !isClickingOnToggle) {
+        navLinks.classList.remove('active');
+    }
+})
